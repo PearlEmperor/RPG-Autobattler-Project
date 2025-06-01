17883197e51849a875d918f90d3396659248c33d
PearlEmperor <64288342+PearlEmperor@users.noreply.github.com>
2025-05-31 17:10:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import * as Warrior from "./warrior.js";
-import * as BasicEnemy from "./basicEnemy.js";
+import * as System from "./systems.js";
 import * as GameState from "./gameStates.js";
+import * as Warrior from "./warrior.js";
+import { InputHandler } from "./inputHandler.js";
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
@@ -9,34 +10,33 @@ window.addEventListener('load', function() {
     canvas.height = 1080;
 
     class Game {
-        constructor(width, height) {
-            this.width = width;
-            this.height = height;
+        constructor(canvas) {
+            this.width = canvas.width;
+            this.height = canvas.height;
+            this.input = new InputHandler(canvas, this);
+
+            this.spawnSpeed = 3; //adjusts how fast the backgrounds/enemies move when spawning a new wave
             this.playerLevel = 1;
-            this.backgrounds = [];
+            this.activeOverlay = 'none';
+
+            //needed in multiple gameStates - easier than trying to pass them between each state that needs them
+            this.currentWave = 1;
+            this.maxEnemies = 1;
 
             //stores the player's party layout and unused heroes - party will later be created/adjusted using player input
-            this.party = [new Warrior.Warrior(this)];
+            this.party = [new Warrior.Paladin(this)];
             this.extraHeroes = [];
-
-            //used in the combat loop, here so they can be easily accessed by combatants during battling gameState
-            this.heroes = [];
-            this.enemies = [];
-            this.combatants = [];
-
-            //extra checks needed for various parts of the combat loop
-            this.battleReady = 0;
-            this.currentWave = 0;
-            this.maxEnemies = 1;
-            this.currentCombatant = 0;
 
             //list of possible spawn points during battle - !! need to figure this out before moving to party management !!
             // this.heroFormation = [{formationX: 50, formationY: 50, column: 'back', row: 'top'}, {formationX: 50, formationY: 100, column: 'back', row: 'middle'}, {formationX: 50, formationY: 150, column: 'back', row: 'bottom'}, {formationX: 150, formationY: 50, column: 'front', row: 'top'}, {formationX: 150, formationY: 100, column: 'front', row: 'middle'}, {formationX: 150, formationY: 150, column: 'front', row: 'bottom'}];
             // this.enemyFormation = [{formationX: 350, formationY: 50, column: 'front', row: 'top'}, {formationX: 350, formationY: 100, column: 'front', row: 'middle'}, {formationX: 350, formationY: 150, column: 'front', row: 'bottom'}, {formationX: 400, formationY: 50, column: 'back', row: 'top'}, {formationX: 400, formationY: 100, column: 'back', row: 'middle'}, {formationX: 400, formationY: 150, column: 'back', row: 'bottom'}];
 
             //Sets initial GameState
-            this.gameStates = [new GameState.SpawningWave(this), new GameState.Battling(this), new GameState.MainMenu(this)];
-            this.currentGameState = this.gameStates[0];
+            this.gameStates = [new GameState.SpawningWave(this),
+                               new GameState.Battling(this),
+                               new GameState.MainMenu(this),
+                               new GameState.NewDungeon(this)];
+            this.currentGameState = this.gameStates[2];
             this.currentGameState.enter();
         }
 
@@ -52,17 +52,11 @@ window.addEventListener('load', function() {
             this.currentGameState = this.gameStates[state];
             this.currentGameState.enter();
         }
-
-        addEnemy() {
-            //generates new enemies for use in battle
-            //will need to read current dungeon state and generate enemies accordingly
-            //move relevant data here from spawningWave gameState
-            this.enemies.push(new BasicEnemy.GoblinFighter(this));
-        }
     }
 
-    const game = new Game(canvas.width, canvas.height);
+    const game = new Game(canvas);
     console.log(game);
+
     let lastTime = 0;
 
     function animate(timeStamp) {
